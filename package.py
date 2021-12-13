d3f6fa3012021aec4b4ba90b727a516a54a4f305
henry <1205543030@qq.com>
2021-12-13 17:19:00 +0800

    60%

--- package.py
+++ package.py
@@ -12,11 +12,16 @@ path_icon_compile = rootPath + 'mergeIcon/icon_compile/'
 path_icon_mergeIcon = rootPath + 'mergeIcon/'
 path_channel_module = rootPath
 path_splash = rootPath + 'mergeIcon/splash/'
-file_icon_name = 'gowan_icon_xyxz.png'
+path_library_platform_plugin = rootPath + 'library_platform_plugin/'
+suffix_res_drawable = '/src/main/res/drawable/'
+file_icon_name = ''
 
 
 # 递归删除 gowan_icon gowan_splash 开头的图片文件
 def deleteFileByLike(path, starsWithStr, endsWithStr=('jpg', 'png', 'jpeg', 'bmp')):
+    if not os.path.exists(path):
+        print('不存在路径：' + path)
+        return
     for file_name in listdir(path):
         new_path = path + '/' + file_name
         if os.path.isdir(new_path) and file_name.startswith('drawable'):
@@ -25,21 +30,35 @@ def deleteFileByLike(path, starsWithStr, endsWithStr=('jpg', 'png', 'jpeg', 'bmp
             os.remove(new_path)
 
 
+# 将加工后的icon复制到对应的渠道资源目录下
 def copyIcon2ChannelModule(channel):
-    channelPath = rootPath + channel + '/' + 'res/' + 'drawable/'  # 需要改
-    if not os.path.exists(channelPath):
-        os.makedirs(channelPath)
-
-    if os.path.exists(path_icon_compile + 'gowan_icon_xyxz_' + channel + '.png'):
-        shutil.copy(path_icon_compile + 'gowan_icon_xyxz_' + channel + '.png', channelPath + 'gowan_icon.png')
+    channelPath = checkPath(channel)
+    if channelPath == "":
+        return
+    # 渠道有角标 则在compile目录中存在加工后的图标，若没有角标则复制直接复制不加工的游戏图标
+    if os.path.exists(path_icon_compile + channel + '.png'):
+        shutil.copy(path_icon_compile + channel + '.png', channelPath + 'gowan_icon.png')
     else:
         shutil.copy(path_icon_mergeIcon + file_icon_name, channelPath + 'gowan_icon.png')
 
 
-def copySplash2ChannelModule(channel):
-    channelPath = rootPath + channel + '/' + 'res/' + 'drawable/'  # 需要改
+# 检测channel.txt 中填写的渠道是否正确: 若渠道模块不存在则返回空，若存在渠道模块再判断是否含有资源目录
+def checkPath(channel):
+    channelPath = path_library_platform_plugin + channel + suffix_res_drawable
     if not os.path.exists(channelPath):
-        os.makedirs(channelPath)
+        if os.path.exists(path_library_platform_plugin + channel):
+            os.makedirs(channelPath)
+        else:
+            return ""
+
+    return channelPath
+
+
+# 将加工后的splash复制到对应的渠道资源目录下
+def copySplash2ChannelModule(channel):
+    channelPath = checkPath(channel)
+    if channelPath == "":
+        return
 
     path_file_jpg = path_splash + channel + '.jpg'
     path_file_png = path_splash + channel + '.png'
@@ -57,14 +76,33 @@ def copySplash2ChannelModule(channel):
     #         shutil.copy(path_icon_compile + file_name, path_destination)
 
 
+def findIconFileName(gameName):
+    for file_name in listdir(path_icon_mergeIcon):
+        if gameName in file_name and os.path.isfile(path_icon_mergeIcon + file_name):
+            return file_name
+
+    return ''
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    print('参数个数为:', len(sys.argv), '个参数。')
-    print('参数列表:', str(sys.argv))
+    # print('参数个数为:', len(sys.argv), '个参数。')
+    # print('参数列表:', str(sys.argv))
+    # if not len(sys.argv) > 1:
+    #     print("请输入游戏参数")
+    #     exit(1)
+    # gameName = sys.argv[1]
+    gameName = 'mrz'
+    iconFileName = findIconFileName(gameName)
+    file_icon_name = iconFileName
+    if iconFileName == '':
+        print('参数错误，找不到对应图标')
+        exit(0)
 
+    # print(sys.argv[1])
     # icon 处理
-    os.system("java -jar mergeIcon/packagetool.jar mergeIcon --icon mergeIcon/gowan_icon_xyxz.png")
+    os.system("java -jar mergeIcon/packagetool.jar mergeIcon --icon mergeIcon/%s" % iconFileName)
     # splash 处理
 
     # my_path = 'C:\Python Pool\Test\\'
@@ -72,17 +110,21 @@ if __name__ == '__main__':
     channel = []
     for line in open("channel.txt", "r"):
         channel.append(line.rstrip())
-        my_path = rootPath + '/' + line.rstrip() + '/res'
+        my_path = path_library_platform_plugin + line.rstrip() + suffix_res_drawable
+        # print(my_path)
         deleteFileByLike(my_path, ('gowan_icon', 'gowan_splash'))
+        copyIcon2ChannelModule(line.rstrip())
+        copySplash2ChannelModule(line.rstrip())
 
     print(path_icon_compile)
     print('---------------------------')
 
-    copyIcon2ChannelModule('57k')
-    copyIcon2ChannelModule('qishizhushou')
-    copySplash2ChannelModule('57k')
-    copySplash2ChannelModule('qishizhushou')
-    copySplash2ChannelModule('leidian')
+    # copyIcon2ChannelModule('57k')
+    # copyIcon2ChannelModule('qishizhushou')
+    # copyIcon2ChannelModule('leidian')
+    # copySplash2ChannelModule('57k')
+    # copySplash2ChannelModule('qishizhushou')
+    # copySplash2ChannelModule('leidian')
     # f.close()  # 关闭文件
     # my_path = os.getcwd() + '/qishizhushou/res'
 
